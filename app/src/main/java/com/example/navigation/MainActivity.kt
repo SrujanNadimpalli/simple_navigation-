738ab0faaf6d3ced2b82b5939593a5a9a29c588f
SrujanNadimpalli <127650135+SrujanNadimpalli@users.noreply.github.com>
2024-03-12 23:57:44 +0530

    simple navigation in kotlin

---
+++ app/src/main/java/com/example/navigation/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.example.navigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener{
+
+    lateinit var  drawer: DrawerLayout
+    lateinit var navigationView: NavigationView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        drawer = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        var actionBarDrawerToggle = ActionBarDrawerToggle(this,drawer,toolbar,
+            R.string.navigation_drawer_open,R.string.navigation_drawer_close)
+
+        drawer.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
+        if(savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, MessageFragment()).commit()
+            navigationView.setCheckedItem(R.id.nav_message)
+        }
+    }
+
+    override fun onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId)
+        {
+            R.id.nav_message -> supportFragmentManager.beginTransaction().replace(R.id.fragment_container,MessageFragment())
+                .commit()
+
+            R.id.nav_chat -> supportFragmentManager.beginTransaction().replace(R.id.fragment_container,ChatFragment())
+                .commit()
+
+            R.id.nav_profile -> supportFragmentManager.beginTransaction().replace(R.id.fragment_container,ProfileFragment())
+                .commit()
+
+            R.id.nav_share -> Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show()
+            R.id.nav_send -> Toast.makeText(this, "Send", Toast.LENGTH_SHORT).show()
+        }
+        drawer.closeDrawer(GravityCompat.START)
+        return true
+
+    }
+}
---
+++ app/src/main/java/com/example/navigation/MessageFragment.kt
@@ -0,0 +1,16 @@
+package com.example.navigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class MessageFragment: Fragment() {
+    lateinit var v: View
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?  ): View? {
+
+        v = inflater.inflate(R.layout.fragment_message,container,false)
+        return  v
+    }
+}
